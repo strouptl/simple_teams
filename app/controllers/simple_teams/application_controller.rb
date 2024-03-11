87f0957024aa3882fd40aafad37bd6de88936e40
Laney Stroup <laney@stroupsolutions.com>
2024-03-12 00:49:47 +0900

    allow configuration of parent_controller and layout;

--- app/controllers/simple_teams/application_controller.rb
+++ app/controllers/simple_teams/application_controller.rb
@@ -1,6 +1,8 @@
 module SimpleTeams
-  class ApplicationController < ActionController::Base
+  class ApplicationController < SimpleTeams.parent_controller
     before_action :authenticate_user!
+
+    layout SimpleTeams.layout
 
     def current_ability
       SimpleTeams::Ability.new(current_user)
--- lib/simple_teams/engine.rb
+++ lib/simple_teams/engine.rb
@@ -4,9 +4,19 @@ module SimpleTeams
   end
 
   mattr_accessor :member_class
+  mattr_accessor :parent_controller
+  mattr_accessor :layout
 
   def self.member_class
     (@@member_class || "User").constantize
+  end
+
+  def self.parent_controller
+    (@@parent_controller || "ApplicationController").constantize
+  end
+
+  def self.layout
+    (@@layout || "simple_teams/application")
   end
 
 end
