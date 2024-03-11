aa0d6239f908392c55bfe52a5c8cf5d5506cb06b
Laney Stroup <laney@stroupsolutions.com>
2024-03-11 14:52:54 +0900

    bundle cancan and scaffold Ability model;

---
+++ app/models/simple_teams/ability.rb
@@ -0,0 +1,34 @@
+module SimpleTeams
+  class Ability
+    include ::CanCan::Ability
+
+    def initialize(user)
+
+      return unless user.present?
+
+      can :create, Team
+
+      # Team
+      can :read, Team do |team|
+        user.member_of_team?(team)
+      end
+
+      can [:update], Team do |team|
+          %w"owner administrator".include? user.role_for_team(team)
+      end
+
+      can [:destroy], Team do |team|
+          %w"owner".include? user.role_for_team(team)
+      end
+
+      # Team Memberships/Invitations
+      can :read, [Membership, Invitation] do |object|
+        user.member_of_team?(object.team)
+      end
+
+      can :manage, [Membership, Invitation] do |object|
+        %w"owner administrator".include? user.role_for_team(object.team)
+      end
+    end
+  end
+end
