7b7b73c100886582188ccec484c55cae4efb0582
Laney Stroup <laney@stroupsolutions.com>
2025-02-03 16:57:48 +0000

    Update role_names/descriptions locales: - Nest names and descriptions under roles - Use plural resource name rather than singular - Nest roles under resource name for custom role names

--- app/helpers/simple_teams/base_helper.rb
+++ app/helpers/simple_teams/base_helper.rb
@@ -1,11 +1,11 @@
 module SimpleTeams
   module BaseHelper
     def team_based_role_name(team, role)
-      t "simple_teams.#{team.teamable.class.to_s.underscore}.role_names.#{role}", :default => [t("simple_teams.role_names.#{role}")]
+      t "simple_teams.#{team.teamable.class.model_name.plural}.roles.names.#{role}", :default => [t("simple_teams.roles.names.#{role}")]
     end
 
     def team_based_role_description(team, role)
-      t "simple_teams.#{team.teamable.class.to_s.underscore}.role_descriptions.#{role}", :default => [t("simple_teams.role_descriptions.#{role}")]
+      t "simple_teams.#{team.teamable.class.model_name.plural}.roles.descriptions.#{role}", :default => [t("simple_teams.roles.descriptions.#{role}")]
     end
   end
 end
